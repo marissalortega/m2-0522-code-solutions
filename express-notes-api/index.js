f474ce635de6d1e74e3a75437702a9177ae1a658
Marissa Ortega <marissaljames@gmail.com>
2022-07-15 22:39:40 +0000

    added code for DELETE and PUT.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,53 +1,148 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+// Clients can GET a list of notes.
+
+app.get('/api/notes', (req, res) => {
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const dataJSON = JSON.parse(data);
+
+    const dataArr = [];
+
+    for (const key in dataJSON.notes) {
+      dataArr.push(dataJSON.notes[key]);
+    }
+
+    res.send(dataArr);
+
+  });
+
+});
+
+// Clients can GET a single note by id.
+
+app.get('/api/notes/:id', (req, res) => {
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    var dataJSON = JSON.parse(data);
+
+    if (req.params.id > 0 && typeof dataJSON.notes[req.params.id] === 'object') {
+      res.status(200).json(dataJSON.notes[req.params.id]);
+    } else if (req.params.id < 0) {
+      res.status(400).json({ error: 'id must be a postive integer' });
+    } else if (req.params.id >= 0 && typeof dataJSON.notes[req.params.id] === 'undefined') {
+      res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+    }
+
+  });
+
+});
+
+// http GET localhost:3000/api/notes
+
+// Clients can POST a new note.
+
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+
+    const dataJSON = JSON.parse(data);
+
+    if (err) throw err;
+
+    else if (Object.keys(req.body).length === 0) {
+      res.status(400).json({ error: 'content is a required field' });
+    } else if (Object.keys(req.body)[0] === 'content') {
+
+      const nextNoteId = dataJSON.nextId++;
+      dataJSON.notes[nextNoteId] = req.body;
+      dataJSON.notes[nextNoteId] = Object.assign({ id: nextNoteId }, dataJSON.notes[nextNoteId]);
+      const newData = JSON.stringify(dataJSON, null, 2);
+
+      fs.writeFile('data.json', newData, err => {
+        if (err) throw err;
+      });
+
+      res.status(201).send(dataJSON.notes[nextNoteId]);
+    }
+
+  // http POST localhost:3000/api/notes
+  });
+});
+
+// Clients can DELETE a note by id.
+app.delete('/api/notes/:id', (req, res) => {
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+
+    const dataJSON = JSON.parse(data);
+
+    if (err) throw err;
+
+    else if (req.params.id <= 0) {
+      res.status(400).json({ error: 'id must be a positive integer' });
+    } else if (req.params.id > 0 && !dataJSON.notes[req.params.id]) {
+      res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+    } else if (req.params.id > 0 && dataJSON.notes[req.params.id]) {
+      delete dataJSON.notes[req.params.id];
+
+      const newData = JSON.stringify(dataJSON, null, 2);
+
+      fs.writeFile('data.json', newData, err => {
+        if (err) throw err;
+      });
+
+      res.sendStatus(204);
+    }
+  });
+});
+
+// http DELETE localhost:3000/api/notes
+
+// Clients can replace a note (PUT) by id.
+
+app.put('/api/notes/:id', (req, res) => {
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    const dataJSON = JSON.parse(data);
+
+    if (err) console.error(err);
+
+    else if (req.params.id <= 0) {
+      res.status(400).json({ error: 'id must be a positive integer' });
+
+    } else if (Object.keys(req.body)[0] !== 'content') {
+      res.status(404).json({ error: 'content is a required field' });
+
+    } else if (req.path !== '/api/notes/:id') {
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+
+    } else if (req.params.id > 0 && Object.keys(req.body)[0] === 'content') {
+      dataJSON.notes[req.params.id].content = req.body.content;
+
+      const newData = JSON.stringify(dataJSON, null, 2);
+
+      fs.writeFile('data.json', newData, err => {
+        if (err) throw err;
+      });
+
+      res.status(200).json(dataJSON.notes[req.params.id]);
+    }
+
+  });
+});
+
+// http PUT localhost:3000/api/notes
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server is listening on port 3000!');
 });
-
-// const nextId = 5;
-
-const notes = {
-  1: {
-    content: 'The event loop is how a JavaScript runtime pushes asynchronous callbacks onto the stack once the stack is cleared.',
-    id: 1
-  },
-  2: {
-    content: 'Prototypal inheritance is how JavaScript objects delegate behavior.',
-    id: 2
-  },
-  3: {
-    content: 'In JavaScript, the value of `this` is determined when a function is called; not when it is defined.',
-    id: 3
-  },
-  4: {
-    content: 'A closure is formed when a function retains access to variables in its lexical scope.',
-    id: 4
-  }
-};
-
-app.get('/api/notes', (req, res) => {
-  const notesArr = [];
-
-  for (const key in notes) {
-    notesArr.push(notes[key]);
-  }
-
-  res.json(notesArr);
-
-});
-
-app.get('/api/notes/:id', (req, res) => {
-
-  if (req.params.id >= 0 && notes[req.params.id] === undefined) {
-    res.status(404).send({ error: `cannot find note with id ${req.params.id}` });
-  } else if (req.params.id <= 0) {
-    res.status(400).send({ error: 'id must be a postive integer' });
-  } else if (req.params.id > 0) {
-    res.status(200).send(notes[req.params.id]);
-  }
-
-});
-
-// http GET localhost:3000/api/notes
